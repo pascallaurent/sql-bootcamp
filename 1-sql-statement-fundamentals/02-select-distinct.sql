2efee8c8cd473b8c1e277d8ff7507bb7f81d901b
pascallaurent <4zwazo@gmail.com>
2021-02-01 13:56:13 -0500

    Query unique districts customers

--- 1-sql-statement-fundamentals/02-select-distinct.sql
+++ 1-sql-statement-fundamentals/02-select-distinct.sql
@@ -1,3 +1,7 @@
 -- we want to know the type of rating
 -- we have in our database
 SELECT DISTINCT(rating) FROM film
+
+-- How many unique districts are our customers from
+SELECT COUNT(DISTINCT(district)),
+FROM address
