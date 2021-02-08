fe1a31ebe2d0cf078f167c3b5e1f0215c098320d
pascallaurent <4zwazo@gmail.com>
2021-02-07 22:40:32 -0500

    different ways of using extract

---
+++ 5-adv-sql-commands/2-extract.sql
@@ -0,0 +1,21 @@
+-- EXTRACT()
+SELECT EXTRACT(YEAR FROM payment_date) AS year
+FROM payment
+
+SELECT EXTRACT(MONTH FROM payment_date) AS year
+FROM payment
+
+SELECT EXTRACT(DAY FROM payment_date) AS year
+FROM payment
+
+SELECT EXTRACT(HOUR FROM payment_date) AS year
+FROM payment
+
+SELECT EXTRACT(MINUTE FROM payment_date) AS year
+FROM payment
+
+SELECT EXTRACT(SECOND FROM payment_date) AS year
+FROM payment
+
+SELECT EXTRACT(QUARTER FROM payment_date) AS year
+FROM payment
